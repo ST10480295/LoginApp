076b37da11dd86fdba41801c9f1e40cd675aeb85
Student <Student@LabVM2049939.teuclq10yh0uvpuvlebo1jkxae.jnbx.internal.cloudapp.net>
2026-09-17 00:43:23 +0200

    Added comment to main class

--- src/loginapp/LoginApp.java
+++ src/loginapp/LoginApp.java
@@ -1,4 +1,4 @@
-/*
+/* // Main class for the Login Application
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
